8bccf447c6e5a84989ce7d548ae281628f77e07c
bogomilmihaylov <bogomilmihaylov@live.com>
2019-08-15 19:53:22 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -12,9 +12,12 @@ end
 
 def find_max_value(array)
   x = array[0]
-  array.length.times do |count|
+  array.length.times do { |count|
   if array[count] > x 
   x = array[count]
+end
+}
+return x 
 end
 
 def find_min_value(array)
